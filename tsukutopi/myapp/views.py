c407046d16813f4ab04e6ff3374e48fdb367c136
小須田侑暉 <YukiK@coins.tsukuba.ac.jp>
2022-12-21 12:18:38 +0900

    入力できるよ

--- tsukutopi/myapp/views.py
+++ tsukutopi/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
 from django.db.models import Q
 from myapp.forms import Profileforms
@@ -16,12 +16,14 @@ def index(request):
 def result(request):
     profile = Profile.objects.filter(Q(key_number=202088888)|Q(key_number=202099999))
     profile_match = Profile.objects.filter(Q(key_number=202088888)|Q(key_number=202099999))
-    major_list = ["総合学類", "情報メディア創生学類", "人文学類", "比較文化学類", "日本語・日本文化学類", "社会学類", "国際総合学類", "教育学類", "心理学類", "障害科学類", "生物学類", "生物資源学類", "地球学類", "数学類", "物理学類", "化学類", "応用理工学類", "工学システム学類", "社会工学類", "情報科学類", "知識情報図書館学類", "医学類", "看護学類", "医療科学類", "体育専門学群", "芸術専門学群"]
-    for i in profile:
-        i.major_sub = major_list[i.major_sub]
+    profile_all = Profile.objects.all()
+    # major_list = ["総合学類", "情報メディア創生学類", "人文学類", "比較文化学類", "日本語・日本文化学類", "社会学類", "国際総合学類", "教育学類", "心理学類", "障害科学類", "生物学類", "生物資源学類", "地球学類", "数学類", "物理学類", "化学類", "応用理工学類", "工学システム学類", "社会工学類", "情報科学類", "知識情報図書館学類", "医学類", "看護学類", "医療科学類", "体育専門学群", "芸術専門学群"]
+    # for i in profile:
+    #     i.major_sub = major_list[i.major_sub]
     context = {
         'profile':profile,
-        'major_list': major_list
+        # 'major_list': major_list,
+        'profile_all':profile_all
     }
     
 
@@ -33,5 +35,6 @@ def kakikomi(request):
         profileform = Profileforms(request.POST)
         if profileform.is_valid():
             profileform.save()
+            return redirect('index')
 
     return render(request, 'myapp/input_profile.html', {'form1': f} )
