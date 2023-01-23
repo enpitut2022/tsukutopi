84b695201dfb01149e0e9d5adc4841b34b4afad9
MiyamotoRin <67382813+MiyamotoRin@users.noreply.github.com>
2023-01-23 19:36:14 +0900

    fix: コンフリクトを修正

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('result', views.result, name="result"),
-<<<<<<< HEAD:myapp/urls.py
-=======
     path('input/',views.kakikomi, name='kakikomi'),
->>>>>>> origin/tk_layout2:tsukutopi/myapp/urls.py
 ]
